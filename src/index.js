bf19e1278e8064e04b7599e018d01ae0db09196b
Milagros <mvilchez@axxonconsulting.com>
2023-01-06 14:43:35 -0300

    saque acc y dataset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 SalesforceInteractions.init({
-  account: "axxonconsultingsa",
-  dataset: "mvtest",
   cookiedomain: "https://interactionmv.netlify.app/",
   trackerUrl: "https://axxonconsultingsa.us-5.evergage.com",
 }).then(() => {
